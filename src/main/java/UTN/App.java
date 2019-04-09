798e17e920800e8074a37eb7c0da1d910c71200d
maurogch <maurogch@gmail.com>
2019-04-08 22:49:52 -0300

    comments on program funcionality

--- src/main/java/UTN/App.java
+++ src/main/java/UTN/App.java
@@ -1,13 +1,16 @@
 package UTN;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //Human
+
+
+        /*Cada ronda pasa esto
+        vikingo1 y barbaro1 llaman a metodos beber y despues orinar, si orinar devuelve true, pierde
+         */
+
+        /*se crea vikingo con un integer (tolerancia) y ese valor se pasa a beber o orinar, dependiendo la implementacion*/
     }
 }
