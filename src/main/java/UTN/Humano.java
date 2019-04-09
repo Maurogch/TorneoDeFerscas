1f090c3413d6d883445703e6636b8d7dae61cc6d
maurogch <maurogch@gmail.com>
2019-04-08 22:49:33 -0300

    Attributes and methods

--- src/main/java/UTN/Humano.java
+++ src/main/java/UTN/Humano.java
@@ -1,4 +1,41 @@
 package UTN;
 
 public class Humano {
+    private String nombre;
+    private Integer edad;
+    private Integer peso;
+    private Orinar orinar;
+    private Beber beber;
+
+    public Humano(String nombre, Integer edad, Integer peso, Orinar orinar, Beber beber) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.orinar = orinar;
+        this.beber = beber;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public Integer getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Integer peso) {
+        this.peso = peso;
+    }
 }
