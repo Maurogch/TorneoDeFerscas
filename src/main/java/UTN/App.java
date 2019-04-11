34f3458a093b592f2531dff15c6647a8c0c088f4
maurogch <maurogch@gmail.com>
2019-04-10 21:17:01 -0300

    Method Enfrentamiento done as POC and a battle done

--- src/main/java/UTN/App.java
+++ src/main/java/UTN/App.java
@@ -1,16 +1,51 @@
 package UTN;
 
-public class App 
+import java.util.concurrent.ThreadLocalRandom;
+
+public class App
 {
     public static void main( String[] args )
     {
-        //Human
+        Humano v = new Vikingo("Asmund Hjorleifsson", 20, 72, new OrinarVikingoImp(), new BeberVikingoImp(), 90);
+        Humano e = new Espartano("Iasonas Giannopoulos", 21, 80, new OrinarEspartanoImp(), new BeberEspartanoImp(), 80);
 
+        try {
+            Humano ganador = Enfrentamiento(v,e);
+            System.out.println("Ganador:" + ganador.getNombre());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 
-        /*Cada ronda pasa esto
-        vikingo1 y barbaro1 llaman a metodos beber y despues orinar, si orinar devuelve true, pierde
-         */
+    /*Very Proof of concert, will change, osea no corrijan esto
 
-        /*se crea vikingo con un integer (tolerancia) y ese valor se pasa a beber o orinar, dependiendo la implementacion*/
+     */
+    public static Humano Enfrentamiento(Humano v, Humano e) throws Exception {
+        if(!(v instanceof Vikingo)){
+            throw new Exception("Primera instancia debe ser del tipo Vikingo");
+        }
+
+        if(!(e instanceof Espartano)){
+            throw new Exception("Primera instancia debe ser del tipo Espartano");
+        }
+
+        int vikingo = 0;
+        int espartano = 0;
+
+        while (vikingo <= 500 && espartano <= 500) {
+            vikingo += v.getBeber().Beber() - ((Vikingo) v).getToleranciaExtra();
+            vikingo += v.getOrinar().Orinar();
+
+            espartano += e.getBeber().Beber();
+            espartano += e.getOrinar().Orinar() - ((Espartano) e).getBebedorProfecional();
+
+            System.out.println("Vikingo: " + vikingo);
+            System.out.println("Espartano: " + espartano);
+        }
+
+        if(vikingo <= 1000)
+            return v;
+        else
+            return e;
     }
 }
