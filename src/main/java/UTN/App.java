63ed80ba478ce0d9d520583b295a26a4d13bfbdd
Mauro <maurogch@gmail.com>
2019-04-15 15:56:07 -0300

    Added more commenting

--- src/main/java/UTN/App.java
+++ src/main/java/UTN/App.java
@@ -1,9 +1,5 @@
 package UTN;
 
-import UTN.Behaviours.BeberEspartanoImp;
-import UTN.Behaviours.BeberVikingoImp;
-import UTN.Behaviours.OrinarEspartanoImp;
-import UTN.Behaviours.OrinarVikingoImp;
 import UTN.Database.JDBC;
 import UTN.Models.*;
 
@@ -16,34 +12,34 @@ import static java.util.Comparator.comparing;
 
 public class App
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) //DNI IMPAR
     {
-        Humano dueno = new VikingoEspartano("Ragnar Leonidas", 40,79, new OrinarEspartanoImp(), new BeberVikingoImp(),15, 15);
+        Humano dueno = new VikingoEspartano("Ragnar Leonidas", 40,79,15, 15);
 
         List<Humano> vikingos = new LinkedList<> (Arrays.<Humano>asList( //force Humano list
-                new Vikingo("Asmund Hjorleifsson", 20, 72, new OrinarVikingoImp(), new BeberVikingoImp(), 8),
-                new Vikingo("Hjort Leiknirsson", 21, 62, new OrinarVikingoImp(), new BeberVikingoImp(), 15),
-                new Vikingo("Asvard Solvisson", 29, 70, new OrinarVikingoImp(), new BeberVikingoImp(), 12),
-                new Vikingo("Hildiglum Slodesson", 27, 74, new OrinarVikingoImp(), new BeberVikingoImp(), 11),
-                new Vikingo("Sighadd Bjalfisson", 24, 61, new OrinarVikingoImp(), new BeberVikingoImp(), 3),
-                new Vikingo("Ljot Skapsson", 31, 84, new OrinarVikingoImp(), new BeberVikingoImp(), 14),
-                new Vikingo("Boe Throstsson", 28, 83, new OrinarVikingoImp(), new BeberVikingoImp(), 13),
-                new Vikingo("Sigewulf Thorleiksson", 32, 72, new OrinarVikingoImp(), new BeberVikingoImp(), 5),
-                new Vikingo("Sigeric Skurfasson", 19, 82, new OrinarVikingoImp(), new BeberVikingoImp(), 7),
-                new Vikingo("Thorleik Jokulsson", 29, 88, new OrinarVikingoImp(), new BeberVikingoImp(), 9)
+                new Vikingo("Asmund Hjorleifsson", 20, 72, 8),
+                new Vikingo("Hjort Leiknirsson", 21, 62, 15),
+                new Vikingo("Asvard Solvisson", 29, 70, 12),
+                new Vikingo("Hildiglum Slodesson", 27, 74, 11),
+                new Vikingo("Sighadd Bjalfisson", 24, 61, 3),
+                new Vikingo("Ljot Skapsson", 31, 84, 14),
+                new Vikingo("Boe Throstsson", 28, 83, 13),
+                new Vikingo("Sigewulf Thorleiksson", 32, 72, 5),
+                new Vikingo("Sigeric Skurfasson", 19, 82, 7),
+                new Vikingo("Thorleik Jokulsson", 29, 88, 9)
         ));
 
         List<Humano> espartanos = new LinkedList<> (Arrays.<Humano>asList( //force Humano list
-                new Espartano("Iasonas Giannopoulos", 21, 80, new OrinarEspartanoImp(), new BeberEspartanoImp(), 7),
-                new Espartano("Haris Xenakis", 27, 73, new OrinarEspartanoImp(), new BeberEspartanoImp(), 2),
-                new Espartano("Christodoulos Savas", 29, 78, new OrinarEspartanoImp(), new BeberEspartanoImp(), 9),
-                new Espartano("Panikos Zervas", 30, 90, new OrinarEspartanoImp(), new BeberEspartanoImp(), 3),
-                new Espartano("Vasso Garis", 20, 67, new OrinarEspartanoImp(), new BeberEspartanoImp(), 10),
-                new Espartano("Isidoros Lekas", 25, 76, new OrinarEspartanoImp(), new BeberEspartanoImp(), 4),
-                new Espartano("Fokionas Gabris", 23, 77, new OrinarEspartanoImp(), new BeberEspartanoImp(), 15),
-                new Espartano("Yiorgos Manikas", 20, 69, new OrinarEspartanoImp(), new BeberEspartanoImp(), 8),
-                new Espartano("Stathis Pateras", 19, 63, new OrinarEspartanoImp(), new BeberEspartanoImp(), 6),
-                new Espartano("Chrysanthos Athanas", 21, 79, new OrinarEspartanoImp(), new BeberEspartanoImp(), 13)
+                new Espartano("Iasonas Giannopoulos", 21, 80, 7),
+                new Espartano("Haris Xenakis", 27, 73, 2),
+                new Espartano("Christodoulos Savas", 29, 78, 9),
+                new Espartano("Panikos Zervas", 30, 90, 3),
+                new Espartano("Vasso Garis", 20, 67, 10),
+                new Espartano("Isidoros Lekas", 25, 76, 4),
+                new Espartano("Fokionas Gabris", 23, 77, 15),
+                new Espartano("Yiorgos Manikas", 20, 69, 8),
+                new Espartano("Stathis Pateras", 19, 63, 6),
+                new Espartano("Chrysanthos Athanas", 21, 79, 13)
         ));
 
         System.out.println("---------------------------Equipo Vikingos----------------------------");
@@ -56,6 +52,7 @@ public class App
         espartanos.forEach(System.out::println);
         System.out.println("-----------------------------------------------------------------------\n");
 
+        //Begin Torunament - Returns winner
         Humano ganador = Torneo(vikingos, espartanos);
 
         if(!Objects.isNull(ganador)){
@@ -64,8 +61,8 @@ public class App
             System.out.println("==========================================================================");
             System.out.println(ganador.toString());
 
+            //Store winner and get winners list
             JDBC.getInstance().setGanador(ganador);
-
             historialDeGanadores();
 
             System.out.println("\n==========================================================================");
@@ -82,12 +79,17 @@ public class App
         }
     }
 
+    public static void historialDeGanadores(){
+        List<Ganador> ganadores = JDBC.getInstance().getGanadores();
+        System.out.println("\n--------HISTORIAL DE GANADORES---------");
+        ganadores.forEach(System.out::println);
+    }
+
     public static Humano Torneo(List<Humano> vikingos, List<Humano> espartanos){
         Humano ganador = null;
 
         try {
             int count = 1;
-
 
             while (vikingos.size() > 0 && espartanos.size() > 0) {
 
@@ -99,12 +101,13 @@ public class App
                 System.out.println("                          VS");
                 System.out.println(e);
 
+                //Individual bout
                 ganador = Enfrentamiento(v,e);
 
                 System.out.println("***Ganador enfrentamiento " + count + ": " + ganador.getClass().getSimpleName() + ", " +
                         ganador.getNombre() + ", Cantidad bebido: " + ganador.getBebidaEnCurpo() + "***");
 
-                if(ganador instanceof Espartano)
+                if(ganador instanceof Espartano) //Delete loser, ie not winner
                     ((LinkedList<Humano>) vikingos).removeFirst();
                 else
                     ((LinkedList<Humano>) espartanos).removeFirst();
@@ -136,6 +139,7 @@ public class App
             */
         }
 
+        //Store aount drank
         participante1.setBebidaEnCurpo(liquidoParticipante1);
         participante2.setBebidaEnCurpo(liquidoParticipante2);
 
@@ -156,6 +160,8 @@ public class App
     public static int Drink(Humano participante){
         int liquido = 0;
 
+        //If not for individual properties of the classes, the implementetion of the strategy would not need the IFs, and
+        //would be 3 lines only
         if(participante instanceof Vikingo){
             liquido += participante.Beber() - ((Vikingo) participante).getBebedorProfecional();
         }else if (participante instanceof VikingoEspartano){
@@ -179,9 +185,4 @@ public class App
         return liquido;
     }
 
-    public static void historialDeGanadores(){
-        List<Ganador> ganadores = JDBC.getInstance().getGanadores();
-        System.out.println("\n--------HISTORIAL DE GANADORES---------");
-        ganadores.forEach(System.out::println);
-    }
 }
